43ce495ad1a703bedf4ceee25d78e9eaf0079787
Deepak Birla <deepakbirla@zuzucodes.com>
2026-04-06 19:09:55 +0530

    update-proposal-form

--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,32 @@
 import HeroSection from "./components/Home/HeroSection";
 import TrustedBy from "./components/common/TrustedBy/TrustedBy";
 import ResultOrientedServices from "./components/Home/ResultOrientedServices";
+import ProposalModal from "./components/auth/ProposalModal";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [isOpen, setIsOpen] = useState(false);
+  useEffect(() => {
+    const timer = null;
+    setTimeout(() => {
+      setIsOpen(true);
+    }, 3000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
   return (
     <>
       <HeroSection />
       <TrustedBy />
       <ResultOrientedServices />
+      <ProposalModal
+        isOpen={isOpen}
+        onClose={() => {
+          setIsOpen(false);
+        }}
+      />
     </>
   );
 }
