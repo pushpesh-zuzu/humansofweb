ebf38acfc0b11a217544e549c25ccafefb7371d2
Deepak Birla <deepakbirla@zuzucodes.com>
2026-04-09 22:19:03 +0530

    update-banner

--- src/app/components/IndustryExpertise/DentalAgency/DentalAgency.jsx
+++ src/app/components/IndustryExpertise/DentalAgency/DentalAgency.jsx
@@ -22,7 +22,7 @@ function DentalAgency() {
       <HeroSectionServices
         heading1="Turn Patient Searches "
         heading2="Into Real Appointments"
-        bgImag="/IndustryExpertise/dentalAgency/dentalHero.webp"
+        bgImag="/IndustryExpertise/dentalAgency/dentalAgencyHero.webp"
         description1="Every day, people search online for trusted dental care — from routine checkups to urgent treatments. The real challenge isn’t just being online, it’s being seen and chosen at the right moment."
         description2="At Humans of Web, we help dental clinics connect with patients who are actively looking for services like yours. Through targeted SEO, local optimization, and smart advertising, we make sure your clinic appears when it matters most. More visibility means more calls, more bookings, and consistent growth you can rely on."
         altText="dentalAgency Agency"
