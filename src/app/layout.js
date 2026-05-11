6220252c12fd4deb56cc55ce909ed7e0b8952594
Deepak Birla <deepakbirla@zuzucodes.com>
2026-05-11 19:20:16 +0530

    update-favicon-icon-name

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,8 +15,8 @@ export const metadata = {
   description:
     "A trusted digital marketing agency in San Jose, How offer bespoke digital marketing services that turn website visitors into paying customers. Call us now!",
   icons: {
-    icon: "/logoIcon.svg",
-    shortcut: "/logoIcon.svg",
+    icon: "/LogoIcon.svg",
+    shortcut: "/LogoIcon.svg",
   },
   openGraph: {
     title: "Boost Your SaaS & eCommerce Growth | How Digital Marketing Experts",
