84e99cb95da05dee0da2eaf8ac5c22d15620ca89
Deepak Birla <deepakbirla@zuzucodes.com>
2026-05-21 16:17:05 +0530

    delete-saas-related-route-and-page

--- src/app/saas-marketing-agency/page.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import SaaS from "../components/IndustryExpertise/SaasAgency/SaaS";
-
-function page() {
-  return <SaaS />;
-}
-
-export default page;
--- src/app/saas-seo/page.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Saas from "../components/IndustryExpertise/SaasAgency/SaaS";
-
-const page = () => {
-  return <Saas />;
-};
-
-export default page;
